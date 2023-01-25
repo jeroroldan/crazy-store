17a2f4380b9f54abcd92256548a222ed99f26b02
Jerónimo Roldán <jeronimoroldan12@gmail.com>
2023-01-25 09:57:23 -0300

    se agrego padding

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           alt="Coffee grinder"
         />
         <Box sx={{ position:'absolute', zIndex:'99', display:'flex', flexDirection:'column',textTransform:'uppercase',marginTop:'5px' ,alignItems:'center',  }}>
-          <Typography variant="h1" component='h1' sx={{ fontSize: '52px', marginTop:'15px', color:'blue' }}>Tienda de Licores Crazy</Typography>
+          <Typography variant="h1" component='h1' sx={{ fontSize: '52px', marginTop:'15px',padding:'10px' ,color:'blue' }}>Tienda de Licores Crazy</Typography>
           <Typography variant="subtitle1"  sx={{ marginTop:'3', zIndex:'99', fontSize:'22px' }}>Prepárate los mejores tragos del <span style={{ fontWeight: 800 }}>Verano</span></Typography>
         </Box>
       {/* <Toolbar sx={{  }}>
