df5dfb470c3535fb2a75596ca301dc65573f21c7
jeronimo-sugal <jeronimoroldan12@gmail.com>
2023-04-14 20:15:25 -0300

    se actualizaron los precios

--- data/products.ts
+++ data/products.ts
@@ -10,14 +10,14 @@ export const products: CrazyProduct[] = [
     type:'Vodka - Bebida blanca',
     title:'vodka Crazy',
     description:'Vodka ideal para el verano',
-    price:600,
+    price:660,
     url: '/vodka.png'
   },
   {
     id:1,
     type:'Rom - Bebida blanca',
     title:'rom Crazy',
-    price:650,
+    price:720,
     description:'Rom ideal prepararte los mejores tragos',
     url: '/rom-crazy.png'
   },
@@ -25,31 +25,55 @@ export const products: CrazyProduct[] = [
     id:2,
     type:'Tekila - Bebida blanca',
     title:'tekila Crazy',
-    price:700,
+    price:720,
     description:'Tekila ideal prepararte los mejores tragos',
     url: '/tekilaCrazy.png'
   },
   {
-    id:4,
+    id:3,
     type:'Gin - Bebida blanca',
     title:'gin Crazy',
-    price:600,
+    price:660,
     description:'Gin ideal prepararte los mejores tragos',
     url: '/gin.jpg'
   },
   {
-    id:3,
-    type:'Licores - Bebida blanca',
-    title:'Tekila Crazy',
+    id:4,
+    type:'Licores ',
+    title:'Licores',
     price:700,
     description:'Licores con todos sus sabores',
     url: '/licores.jpg'
   },
   {
+    id:5,
+    type:'Licores ',
+    title:'Pina Colada',
+    price:800,
+    description:'Licores con todos sus sabores',
+    url: '/licores.jpg'
+  },
+  {
     id:6,
+    type:'Licores ',
+    title:'Granadina',
+    price:745,
+    description:'Licores con todos sus sabores',
+    url: '/licores.jpg'
+  },
+  {
+    id:7,
     type:'Petakas - Bebida blanca',
     title:'petakas Crazy',
-    price:210,
+    price:230,
+    description:'Infaltables en tu negocio',
+    url: '/petakas.jpg'
+  },
+  {
+    id:8,
+    type:'Petakas - Bebida cremosa',
+    title:'petakas  Cremosas',
+    price:260,
     description:'Infaltables en tu negocio',
     url: '/petakas.jpg'
   },
