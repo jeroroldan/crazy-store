8d67adbc33aed751b0794d6b4d0c4adea2d2cc6c
jeronimo-sugal <jeronimoroldan12@gmail.com>
2023-04-22 00:46:36 -0300

    cambios

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { JsxElement } from 'typescript';
 import Navbar from '../ui/Navbar';
 import PAGE_TRANSITION from '@/utils/pageTransitionsVars';
-import { LazyMotion , domAnimation, m  } from 'framer-motion';
+
 
 
 const pageMotionProps = {
@@ -38,46 +38,44 @@ interface Props {
 export const ShopLayout:FC<Props> = ({ children, title, pageDescription, imageFullUrl }) => {
     return (
         <>
-            <LazyMotion features={domAnimation}>
             
-                <Head>
-                    <title>{ title }</title>
+            <Head>
+                <title>{ title }</title>
 
-                    <meta name="description" content={ pageDescription } />
-                    
-                    
-                    <meta name="og:title" content={ title } />
-                    <meta name="og:description" content={ pageDescription } />
-
-                    {
-                        imageFullUrl && (
-                            <meta name="og:image" content={ imageFullUrl } />
-                        )
-                    }
-
-                </Head> 
-
-                <m.nav>
-                    <Navbar />
-                </m.nav>
-
-                <m.main style={{
-                    margin: '16px auto',
-                    maxWidth: '1440px',
-                    padding: '0px 30px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems:'center'
-                }}>
-                    { children }
-                </m.main>
+                <meta name="description" content={ pageDescription } />
                 
-                {/* Footer */}
-                <m.footer>
-                    {/* TODO: mi custom footer */}
-                </m.footer>
-            </LazyMotion>
+                
+                <meta name="og:title" content={ title } />
+                <meta name="og:description" content={ pageDescription } />
+
+                {
+                    imageFullUrl && (
+                        <meta name="og:image" content={ imageFullUrl } />
+                    )
+                }
+
+            </Head> 
+
+            <nav>
+                <Navbar />
+            </nav>
+
+            <main style={{
+                margin: '16px auto',
+                maxWidth: '1440px',
+                padding: '0px 30px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems:'center'
+            }}>
+                { children }
+            </main>
+            
+            {/* Footer */}
+            <footer>
+                {/* TODO: mi custom footer */}
+            </footer>
 
         </>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { lightTheme } from '@/themes'
 import { CssBaseline } from '@mui/material'
-import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
 
     return (
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
 
-        <AnimatePresence mode='wait' initial={ false }>
-          <Component {...pageProps} key={ router.asPath } />
-        </AnimatePresence>
+        
+          <Component {...pageProps} />
+        
       
       
       </ThemeProvider>
