640c5adb3ea11e9c21c9f65e06c5647be5833f1e
jeronimo-sugal <jeronimoroldan12@gmail.com>
2023-04-22 13:00:18 -0300

    mas cambios

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
 import React from 'react'
-import { Button } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import ProductDetail from '@/components/layouts/ProductDetail'
 import { ShopLayout } from '@/components/layouts';
 import products from "@/data/products"
 import Link from 'next/link';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import DoneIcon from '@mui/icons-material/Done';
 
 export const PageDetailProduct = () => {
 
@@ -26,6 +27,34 @@ export const PageDetailProduct = () => {
         </Button>
       </Link>
       <ProductDetail product={products[valueId|| 0]} />
+
+      <Grid container style={{ display:'flex' , flexDirection:'column' }}>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Mejor precio del mercado</Typography>
+        </Grid>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Alto margen de ganacia</Typography>
+        </Grid>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Vencimento prolongado</Typography>
+        </Grid>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Productos demandados</Typography>
+        </Grid>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Calidad superior</Typography>
+        </Grid>
+        <Grid item style={{ display: 'flex' , justifyContent:'center', margin:'auto' }}>
+          <DoneIcon color='info'></DoneIcon>
+          <Typography>Alta rotación</Typography>
+        </Grid>
+      </Grid>
+
     </ShopLayout>
   )
 }
