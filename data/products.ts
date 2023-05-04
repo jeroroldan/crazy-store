b5e5c73efe6aff94bd01be9ea10e4e34ec1040f0
jeronimo-sugal <jeronimoroldan12@gmail.com>
2023-05-04 13:44:05 -0300

    se cambio la descripcion

--- data/products.ts
+++ data/products.ts
@@ -4,83 +4,79 @@ import { CrazyProduct } from '@/models'
 
 
 export const products: CrazyProduct[] = [
-
   {
-    id:0,
-    type:'Vodka - Bebida blanca',
-    title:'vodka Crazy',
-    description:'Vodka ideal para el invierno',
-    price:750,
-    url: '/vodka.png'
+    id: 0,
+    type: "Vodka - Bebida blanca",
+    title: "vodka Crazy x1L.",
+    description: "Vodka de Calidad superior caja x 6 uni.",
+    price: 750,
+    url: "/vodka.png",
   },
   {
-    id:1,
-    type:'Rom - Bebida blanca',
-    title:'rom Crazy',
-    price:820,
-    description:'Rom ideal prepararte los mejores tragos',
-    url: '/rom-crazy.png'
+    id: 1,
+    type: "Rom - Bebida blanca",
+    title: "rom Crazy x1L.",
+    price: 820,
+    description: "Rom de Calidad superior caja x 6 uni.",
+    url: "/rom-crazy.png",
   },
   {
-    id:2,
-    type:'Tekila - Bebida blanca',
-    title:'tekila Crazy',
-    price:820,
-    description:'Tekila ideal prepararte los mejores tragos',
-    url: '/tekilaCrazy.png'
+    id: 2,
+    type: "Tekila - Bebida blanca",
+    title: "tekila Crazy x1L.",
+    price: 820,
+    description: "Tekila de Calidad superior caja x 6 uni.",
+    url: "/tekilaCrazy.png",
   },
   {
-    id:3,
-    type:'Gin - Bebida blanca',
-    title:'gin Crazy',
-    price:820,
-    description:'Gin ideal prepararte los mejores tragos',
-    url: '/gin.jpg'
+    id: 3,
+    type: "Gin - Bebida blanca",
+    title: "gin Crazy x1L.",
+    price: 820,
+    description: "Gin de Calidad superior caja x 6 uni.",
+    url: "/gin.jpg",
   },
   {
-    id:4,
-    type:'Licores ',
-    title:'Licores',
-    price:800,
-    description:'Licores con todos sus sabores',
-    url: '/licores.jpg'
+    id: 4,
+    type: "Licores ",
+    title: "Licores x1L.",
+    price: 800,
+    description: "Licores de Calidad superior caja x 6 uni.",
+    url: "/licores.jpg",
   },
   {
-    id:5,
-    type:'Licores ',
-    title:'Piña Colada',
-    price:950,
-    description:'Licores con todos sus sabores',
-    url: '/pinacolada.PNG'
+    id: 5,
+    type: "Licores ",
+    title: "Piña Colada x1L.",
+    price: 950,
+    description: "Licores de Calidad superior caja x 6 uni.",
+    url: "/pinacolada.PNG",
   },
   {
-    id:6,
-    type:'Licores ',
-    title:'Granadina',
-    price:975,
-    description:'Licores con todos sus sabores',
-    url: '/licores.jpg'
+    id: 6,
+    type: "Licores ",
+    title: "Granadina",
+    price: 975,
+    description: "Licores de Calidad superior caja x 6 uni.",
+    url: "/licores.jpg",
   },
   {
-    id:7,
-    type:'Petakas - Bebida blanca',
-    title:'petakas Crazy',
-    price:270,
-    description:'Infaltables en tu negocio',
-    url: '/petakaBlanca.jpg'
+    id: 7,
+    type: "Petakas - Bebida blanca",
+    title: "petakas Crazy",
+    price: 270,
+    description: "Infaltables en tu negocio",
+    url: "/petakaBlanca.jpg",
   },
   {
-    id:8,
-    type:'Petakas - Bebida cremosa',
-    title:'petakas  Cremosas',
-    price:300,
-    description:'Infaltables en tu negocio',
-    url: '/petakasPesadas.jpg'
+    id: 8,
+    type: "Petakas - Bebida cremosa",
+    title: "petakas  Cremosas",
+    price: 300,
+    description: "Infaltables en tu negocio",
+    url: "/petakasPesadas.jpg",
   },
-  
-
-
-]
+];
 
 
 export default products;
