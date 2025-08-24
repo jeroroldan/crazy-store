169091de335fd9c5f55e8f5b7181bd290e91152b
Jeronimo Roldan <jeronimo.roldan@alliansys.com.ar>
2025-08-24 20:20:24 -0300

    feat: Se actualizaron los precios.

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,7 @@ const WhatsAppContainer = styled(Box)(({ theme }) => ({
   "&:hover": {
     backgroundColor: theme.palette.success.light,
     transform: "translateY(-2px)",
-    boxShadow: theme.shadows[4],
+    boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)", // Cambia esta línea
   },
 }));
 
